19814c9a23ae60b84c325831ecf0c5a84eee4785
Wormhole616 <144727575+Wormhole616@users.noreply.github.com>
2024-04-28 20:55:36 -0700

    added routes and created package json

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,12 @@
+
 // Imports
 const router = require("express").Router();
-const apiRoutes = require("./api");
+const userRoutes = require("./userRoutes");
+const thoughtRoutes = require("./thoughtRoutes");
 
 // Middleware
-router.use("/api", apiRoutes);
-
-router.use((req, res) => res.send("Wrong route!"));
+router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
 
 // Exports
 module.exports = router;
